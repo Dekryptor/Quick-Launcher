8dc6ccb1a941501c607807a551ed4e0ca18a73bc
Fusionn <fusiontreetwitch@gmail.com>
2016-05-24 04:57:45 -0400

    PPPPPPPPPUUUUUUUSHHHHHHHHHH
    I got bored

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace BetterLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.altis = new System.Windows.Forms.PictureBox();
+            this.ts = new System.Windows.Forms.PictureBox();
+            this.site = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.altis)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.site)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // altis
+            // 
+            this.altis.Location = new System.Drawing.Point(12, 72);
+            this.altis.Name = "altis";
+            this.altis.Size = new System.Drawing.Size(464, 81);
+            this.altis.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.altis.TabIndex = 0;
+            this.altis.TabStop = false;
+            this.altis.DoubleClick += new System.EventHandler(this.clickHandler);
+            // 
+            // ts
+            // 
+            this.ts.Location = new System.Drawing.Point(12, 159);
+            this.ts.Name = "ts";
+            this.ts.Size = new System.Drawing.Size(464, 81);
+            this.ts.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ts.TabIndex = 1;
+            this.ts.TabStop = false;
+            this.ts.DoubleClick += new System.EventHandler(this.clickHandler);
+            // 
+            // site
+            // 
+            this.site.Location = new System.Drawing.Point(12, 246);
+            this.site.Name = "site";
+            this.site.Size = new System.Drawing.Size(464, 81);
+            this.site.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.site.TabIndex = 2;
+            this.site.TabStop = false;
+            this.site.DoubleClick += new System.EventHandler(this.clickHandler);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 338);
+            this.Controls.Add(this.site);
+            this.Controls.Add(this.ts);
+            this.Controls.Add(this.altis);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Easy\'s Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.altis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.site)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox altis;
+        private System.Windows.Forms.PictureBox ts;
+        private System.Windows.Forms.PictureBox site;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetterLauncher
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaterialSkinManager _manager = MaterialSkinManager.Instance;
+            _manager.AddFormToManage(this);
+            _manager.Theme = MaterialSkinManager.Themes.DARK;
+            _manager.ColorScheme = new ColorScheme(Primary.dark, Primary.dark, Primary.dark, Accent.Red200, TextShade.WHITE);
+
+            altis.Load("https://arma3-servers.net/server/31593/banners/banner-3.png");
+            ts.Load("http://teamspeak-servers.org/server/6807/banners/banner-3.png");
+            site.Load("https://i.gyazo.com/dd2cd55f269d54c50a14cd4d76bfe73e.png");
+        }
+
+        private void clickHandler(object sender, EventArgs e)
+        {
+            string _name = ((PictureBox)sender).Name;
+            switch(_name)
+            {
+                case "altis":
+                    System.Diagnostics.Process.Start("steam://rungameid/107410//+connect 162.248.92.58:2302");
+                    break;
+                case "ts":
+                    System.Diagnostics.Process.Start("ts3server://ts3.a3lrp.com?port=9987");
+                    break;
+                case "site":
+                    System.Diagnostics.Process.Start("http://forum.a3lrp.com");
+                    break;
+            }
+        }
+    }
+}
