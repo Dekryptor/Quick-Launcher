5476b7b1cf6ab51f4d46b943af72ab270be93c54
Fusionn <fusiontreetwitch@gmail.com>
2016-05-24 05:24:23 -0400

    Messedup

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -76,7 +76,10 @@
             this.Controls.Add(this.ts);
             this.Controls.Add(this.altis);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(488, 338);
+            this.MinimumSize = new System.Drawing.Size(488, 338);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Easy\'s Launcher";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.altis)).EndInit();
